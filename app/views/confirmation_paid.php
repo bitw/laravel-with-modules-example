80a4faff159c566ed9f418ba7e75d3f557a1b37b
bitw <endval@gmail.com>
2014-02-28 00:40:59 +0700

    update form for foreigner

--- app/views/confirmation_paid.php
+++ app/views/confirmation_paid.php
@@ -1,9 +1,7 @@
 <!--form action="https://merchant.roboxchange.com/Index.aspx" method=POST-->
 <form action="http://test.robokassa.ru/Index.aspx" method=POST>
-
     <h3>Payment for: <?=$order->name?>&nbsp;<?=$order->surname?></h3>
     <h4>Amount: <strong><?=$out_summ?></strong> &euro;</h4>
-
     <input type=hidden name=MrchLogin value="<?=$mrh_login?>"/>
     <input type=hidden name=OutSum value="<?=$out_summ?>"/>
     <input type=hidden name=InvId value="<?=$inv_id?>"/>
@@ -11,10 +9,6 @@
     <input type=hidden name=SignatureValue value="<?=$crc?>"/>
     <input type=hidden name=IncCurrLabel value="<?=$in_curr?>"/>
     <input type=hidden name=Culture value="<?=$culture?>"/>
-
     <input type=hidden name=Shp_paid_key value="<?=$shp_paid_key?>"/>
-    <input type=hidden name=Shp_user_email value="<?=$order->email?>"/>
-
-    <button class="btn btn-default" type="button" onclick="window.location.href='<?=route('kiform.cancel_check', array('key'=>$shp_paid_key, 'email'=>$order->email))?>'">Cancel</button>&nbsp;<button class="btn btn-primary" type="submit">Go to payment</button>
-
+    <button class="btn btn-default" type="button" onclick="window.location.href='<?=route('kiform.cancel_check', array('key'=>$shp_paid_key))?>'">Cancel</button>&nbsp;<button class="btn btn-primary" type="submit">Go to payment</button>
 </form>
