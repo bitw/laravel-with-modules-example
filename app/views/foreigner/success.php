b39a2d3ce8e847b294f56f41e6e10af392c75d4e
bitw <endval@gmail.com>
2014-02-28 11:17:27 +0700

    update view response of success payment

--- app/views/foreigner/success.php
+++ app/views/foreigner/success.php
@@ -1,7 +1,7 @@
 <?if($result == -1):?>
     <h3>Account will not find</h3>
-<?else if($result == 0):?>
+<?elseif($result == 0):?>
     <h3>Account of non-payment or even still in the processing queue.</h3>
-<?else?>
+<?else:?>
     <h3>Проздавля! Плаота успеншо парлша!</h3>
 <?endif?>
